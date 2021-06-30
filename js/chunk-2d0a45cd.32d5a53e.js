d6c5eea33bc28c862c93c083edb280862172d326
孟鹏宇 <mpy98128@gmail.com>
2021-06-30 10:53:26 +0800

    修改

--- js/chunk-2d0a45cd.32d5a53e.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a45cd"],{"05b4":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("order-detail")])},l=[],r={},u=r,a=t("2877"),d=Object(a["a"])(u,c,l,!1,null,null,null);n["default"]=d.exports}}]);
